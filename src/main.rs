e73c7ae1b6b18edc94d9092729b95228fa6774e6
Coddeus <codeusbonus@gmail.com>
2023-05-12 20:30:24 +0200

    📽 Fully working output mp4. ♻ Set the rendering process to a concatenation of one-frame videos 🚛 Moved all code to main function (for a mutable reference)

--- src/main.rs
+++ src/main.rs
@@ -12,13 +12,13 @@ use piston::event_loop::{EventSettings, Events};
 use piston::input::{RenderArgs, RenderEvent, UpdateArgs, UpdateEvent};
 use piston::window::WindowSettings;
 use gl::types::*;
-use png::Encoder;
-use std::fs::File;
-use std::io::{BufWriter, Write};
+use std::fs::{File, create_dir, remove_dir_all};
+use std::io::Write;
 use std::process::{Command, Stdio};
 
 const WIDTH: u32 = 1920; 
 const HEIGHT: u32 = 1080; 
+const FRAMERATE: f32 = 60.0; 
 const ALL: usize = 4 * WIDTH as usize * HEIGHT as usize;
 
 pub struct App {
@@ -55,152 +55,15 @@ impl App {
     }
 }
 
-// fn save_frame(frame_counter: &mut u32) {
-//     let mut pixel_data: Vec<u8> = vec![0; ALL];
-// 
-//     unsafe {
-//         gl::ReadBuffer(gl::FRONT);
-//         gl::ReadPixels(
-//             0,
-//             0,
-//             WIDTH as GLint,
-//             HEIGHT as GLint,
-//             gl::RGBA,
-//             gl::UNSIGNED_BYTE,
-//             pixel_data.as_mut_ptr() as *mut GLvoid,
-//         );
-//     }
-//     
-//     let filename = format!("images/frame_{:010}.png", frame_counter);
-//     {
-//         let file = File::create(filename).unwrap();
-//         let ref mut w = BufWriter::new(file);
-//         let mut encoder = Encoder::new(w, WIDTH, HEIGHT);
-//         encoder.set_color(png::ColorType::Rgba);
-//         encoder.set_depth(png::BitDepth::Eight);
-//         let mut writer = encoder.write_header().unwrap();
-//         writer.write_image_data(&pixel_data).unwrap();
-//     };
-// 
-//     println!("Saving frame: {}", frame_counter);
-//     *frame_counter += 1;
-// 
-// }
-// raw data to video (1 frame) via ffmpeg pipe => concat video to main video.
-fn render_ffmpeg() {
-    // let ffmpeg = Command::new("ffmpeg")
-    //     .arg("-y")
-    //     .arg("-r")
-    //     .arg("60")
-    //     .arg("-s")
-    //     .arg(format!("{}x{}", WIDTH, HEIGHT))
-    //     .arg("-f")
-    //     .arg("image2")
-    //     .arg("-i")
-    //     .arg("./images/frame_%10d.png")
-    //     .arg("-c:v")
-    //     .arg("libx264")
-    //     .arg("-pix_fmt")
-    //     .arg("rgba")
-    //     .arg("-crf")
-    //     .arg("20")
-    //     .arg("-preset")
-    //     .arg("fast")
-    //     .arg("output.mp4")
-    //     .output()
-    //     .expect("Failed to start FFmpeg");
-
-    let mut pixel_data: Vec<u8> = vec![0; ALL];
-
-    unsafe {
-        gl::ReadBuffer(gl::FRONT);
-        gl::ReadPixels(
-            0,
-            0,
-            WIDTH as GLint,
-            HEIGHT as GLint,
-            gl::RGBA,
-            gl::UNSIGNED_BYTE,
-            pixel_data.as_mut_ptr() as *mut GLvoid,
-        );
-    }
-    let mut ffmpeg = Command::new("ffmpeg")
-        .arg("-loglevel")
-        .arg("quiet")
-        .arg("-f")
-        .arg("rawvideo")
-        .arg("-pix_fmt")
-        .arg("rgba")
-        .arg("-s")
-        .arg(format!("{}x{}", WIDTH, HEIGHT))
-        .arg("-i")
-        .arg("-")
-        .arg("-c:v")
-        .arg("libvpx-vp9")
-        .arg("-r")
-        .arg("1")
-        .arg("-frames:v")
-        .arg("1")
-        .arg("-y")
-        .arg("temp.webm")
-        .stdin(Stdio::piped())
-        .spawn()
-        .expect("Failed to start ffmpeg");
-
-    if let Some(ref mut stdin) = ffmpeg.stdin {
-        stdin.write_all(&pixel_data)
-            .expect("Failed to write to stdin");
-    }
-
-    Command::new("ffmpeg")
-        .arg("-loglevel")
-        .arg("quiet")
-        .arg("-i")
-        .arg(format!("concat:{}|{}", "output.webm", "temp.webm"))
-        .arg("-c")
-        .arg("copy")
-        .arg("-y")  // Overwrite output file if it exists
-        .arg("output.webm")
-        .output()
-        .expect("Failed to execute ffmpeg");
-
-
-}
-
 fn main() {
-    // Create the needed files for ffmpeg
-    File::create("temp.webm")
-    .expect("Error encountered while creating temp file.");
-    File::create("output.webm")
-    .expect("Error encountered while creating output file!");
-
-    // TODO
-    // Command::new("ffmpeg")
-    //     .arg("-loglevel")
-    //     .arg("quiet")
-    //     .arg("-f")
-    //     .arg("rawvideo")
-    //     .arg("-pix_fmt")
-    //     .arg("rgba")
-    //     .arg("-s")
-    //     .arg(format!("{}x{}", WIDTH, HEIGHT))
-    //     .arg("-i")
-    //     .arg("-")
-    //     .arg("-c:v")
-    //     .arg("libvpx-vp9")
-    //     .arg("-r")
-    //     .arg("1")
-    //     .arg("-frames:v")
-    //     .arg("1")
-    //     .arg("-y")
-    //     .arg("output.webm")
-    //     .stdin(Stdio::piped())
-    //     .spawn()
-    //     .expect("Failed to start ffmpeg");
+    remove_dir_all("temp").unwrap();
+    create_dir("temp").unwrap();
+    let mut indexfile = File::create("index.txt").unwrap();
+    let mut filename;
 
     let opengl = OpenGL::V3_3;
+    let mut pixel_data: Vec<u8> = vec![0; ALL];
 
-    // Create a Glutin window.
     let mut window: Window = WindowSettings::new("spinning-square", (WIDTH, HEIGHT))
         .graphics_api(opengl)
         .resizable(false)
@@ -211,7 +74,6 @@ fn main() {
     gl_loader::init_gl();
     gl::load_with(|symbol| gl_loader::get_proc_address(symbol) as *const _);
 
-    // Create a new game and run it.
     let mut app = App {
         gl: GlGraphics::new(opengl),
         rotation: 0.0,
@@ -222,8 +84,49 @@ fn main() {
     while let Some(e) = events.next(&mut window) {
         if let Some(args) = e.render_args() {
             app.render(&args);
-            render_ffmpeg();
-            // save_frame(&mut frame_counter);
+
+            // Render the frame to an ffmpeg video
+            unsafe {
+                gl::ReadBuffer(gl::FRONT);
+                gl::ReadPixels(
+                    0,
+                    0,
+                    WIDTH as GLint,
+                    HEIGHT as GLint,
+                    gl::RGBA,
+                    gl::UNSIGNED_BYTE,
+                    pixel_data.as_mut_ptr() as *mut GLvoid,
+                );
+            }
+            let name = format!("temp/{:010}.mp4", frame_counter);
+            filename = name.as_str();
+            let mut ffmpeg = Command::new("ffmpeg")
+                .arg("-loglevel")
+                .arg("16")
+                .arg("-f")
+                .arg("rawvideo")
+                .arg("-pix_fmt")
+                .arg("rgba")
+                .arg("-s")
+                .arg(format!("{}x{}", WIDTH, HEIGHT))
+                .arg("-i")
+                .arg("-")
+                .arg("-vcodec")
+                .arg("libx264")
+                .arg("-crf")
+                .arg("23")
+                .arg("-t")
+                .arg(format!("{}", 1.0/FRAMERATE))
+                .arg("-y")
+                .arg(filename)
+                .stdin(Stdio::piped())
+                .spawn()
+                .unwrap();
+            if let Some(ref mut stdin) = ffmpeg.stdin {
+                stdin.write_all(&pixel_data).unwrap();
+            }
+            writeln!(indexfile, "file {}", filename).unwrap();
+
             println!("Saving frame: {}", frame_counter);
             frame_counter += 1;
         }
@@ -232,4 +135,20 @@ fn main() {
             app.update(&args);
         }
     }
+
+    Command::new("ffmpeg")
+        .arg("-loglevel")
+        .arg("0")
+        .arg("-f")
+        .arg("concat")
+        .arg("-i")
+        .arg("index.txt")
+        .arg("-c")
+        .arg("copy")
+        .arg("-y")
+        .arg("output.mp4")
+        .output()
+        .unwrap();
+
+    println!("\nFresh video generated!\n");
 }
