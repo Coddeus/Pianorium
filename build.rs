ed381e015aa4d2bcedc77da9192af33dbc6a789d
Coddeus <codeusbonus@gmail.com>
2023-08-24 13:05:00 +0200

    🔧 Small fixes for each OS

---
+++ build.rs
@@ -0,0 +1,9 @@
+
+fn main() {
+    #[cfg(target_os = "linux")]
+    println!("cargo:rustc-link-search=\"/usr/lib\"");
+    #[cfg(target_os = "macos")]
+    println!("cargo:rustc-link-search=\"/usr/local/Cellar/sdl2/2.28.2/lib\"");
+    #[cfg(target_os = "windows")]
+    println!("cargo:rustc-link-search=\"./ffmpeg/lib\"");
+}
