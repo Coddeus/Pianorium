358bd54ff4421b96139fe93f7e23e74c3c469e1c
Coddeus <codeusbonus@gmail.com>
2023-08-25 17:48:14 +0200

    🔧 Antislashes ?

--- build.rs
+++ build.rs
@@ -3,5 +3,5 @@ fn main() {
     #[cfg(target_os = "macos")]
     println!("cargo:rustc-link-search=\"/usr/local/Cellar/sdl2/2.28.2/lib\"");
     #[cfg(target_os = "windows")]
-    println!("cargo:rustc-link-search=\"D:/a/Pianorium/Pianorium/SDL2-2.28.2/x86_64-w64-mingw32/lib\"");
+    println!("cargo:rustc-link-search=\"D:\\a\\Pianorium\\Pianorium\\SDL2-2.28.2\\x86_64-w64-mingw32\\lib\"");
 }
