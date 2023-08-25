ef9100c74c4008ea28007815c39b207add7a9299
Coddeus <codeusbonus@gmail.com>
2023-08-25 13:06:43 +0200

    🔧 Linux FFmpeg build & MacOS removed bug command

--- build.rs
+++ build.rs
@@ -1,9 +1,8 @@
 
 fn main() {
-    #[cfg(target_os = "linux")]
-    println!("cargo:rustc-link-search=\"/usr/lib\"");
-    #[cfg(target_os = "macos")]
-    println!("cargo:rustc-link-search=\"/usr/local/Cellar/sdl2/2.28.2/lib\"");
+    // #[cfg(target_os = "macos")]
+    // println!("cargo:rustc-link-search=\"/usr/local/Cellar/sdl2/2.28.2/lib\"");
     #[cfg(target_os = "windows")]
-    println!("cargo:rustc-link-search=\"./sdl2-2.28.2/lib\"");
+    println!("cargo:rustc-link-search=\"./sdl2-2.28.2/build/lib\"");
+    println!("cargo:rustc-link-search=\"./ffmpeg\"");
 }
