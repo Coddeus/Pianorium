6230bfd0456bceb6a3472a9ed103528ad25fccdc
Coddeus <codeusbonus@gmail.com>
2023-08-25 16:29:57 +0200

    🔧 Is rustc-link-search unique ?

--- build.rs
+++ build.rs
@@ -3,6 +3,6 @@ fn main() {
     #[cfg(target_os = "macos")]
     println!("cargo:rustc-link-search=\"/usr/local/Cellar/sdl2/2.28.2/lib\"");
     #[cfg(target_os = "windows")]
-    println!("cargo:rustc-link-search=\"./sdl2-2.28.2\"");
     println!("cargo:rustc-link-search=\"./ffmpeg/lib\"");
+    println!("cargo:rustc-link-search=\"./sdl2-2.28.2\"");
 }
