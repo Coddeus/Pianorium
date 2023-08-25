2dda9722def76048dbd91d7fb99211f9b1389cd9
Coddeus <codeusbonus@gmail.com>
2023-08-25 14:37:18 +0200

    🔧 Dependencies/link-search path for each platform

--- build.rs
+++ build.rs
@@ -1,8 +1,10 @@
 
 fn main() {
-    // #[cfg(target_os = "macos")]
-    // println!("cargo:rustc-link-search=\"/usr/local/Cellar/sdl2/2.28.2/lib\"");
+    #[cfg(target_os = "linux")]
+    println!("cargo:rustc-link-search=\"/home/runner/ffmpeg_build/lib/\"");
+    #[cfg(target_os = "macos")]
+    println!("cargo:rustc-link-search=\"/usr/local/Cellar/sdl2/2.28.2/lib\"");
     #[cfg(target_os = "windows")]
-    println!("cargo:rustc-link-search=\"./sdl2-2.28.2/build/lib\"");
+    println!("cargo:rustc-link-search=\"./sdl2-2.28.2\"");
     println!("cargo:rustc-link-search=\"./ffmpeg\"");
 }
