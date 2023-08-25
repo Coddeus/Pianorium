8ab6efeddcaf587f213b4fe4aad0ddddead388c4
Coddeus <codeusbonus@gmail.com>
2023-08-25 18:01:54 +0200

    🔧 Copy SDL libs inside .rustup/

--- build.rs
+++ build.rs
@@ -2,6 +2,4 @@
 fn main() {
     #[cfg(target_os = "macos")]
     println!("cargo:rustc-link-search=\"/usr/local/Cellar/sdl2/2.28.2/lib\"");
-    #[cfg(target_os = "windows")]
-    println!("cargo:rustc-link-search=\"D:\\a\\Pianorium\\Pianorium\\SDL2-2.28.2\\x86_64-w64-mingw32\\lib\"");
 }
