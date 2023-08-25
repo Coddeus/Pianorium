979cd4dd9c14436d100c4240fad71e395f54a2bc
Coddeus <codeusbonus@gmail.com>
2023-08-25 15:16:35 +0200

    🔧 Linux prebuilt, MacOS sudo, Windows "dir  /s"

--- build.rs
+++ build.rs
@@ -1,7 +1,5 @@
 
 fn main() {
-    #[cfg(target_os = "linux")]
-    println!("cargo:rustc-link-search=\"/home/runner/ffmpeg_build/lib/\"");
     #[cfg(target_os = "macos")]
     println!("cargo:rustc-link-search=\"/usr/local/Cellar/sdl2/2.28.2/lib\"");
     #[cfg(target_os = "windows")]
