41186230e0f7642f6dcd325274b173bca5e792b1
Coddeus <codeusbonus@gmail.com>
2023-08-27 00:38:44 +0200

    🔧 Fixed ps1 path creation

--- script/windows/env-path.ps1
+++ script/windows/env-path.ps1
@@ -1,6 +1,6 @@
 cd D:\a\Pianorium\Pianorium
 
-mkdir -p deps\source\x264 & mkdir -p deps\source\ffmpeg & mkdir -p deps\build\x264 & mkdir -p deps\build\ffmpeg & mkdir -p deps\installed
+mkdir -p deps\source\x264 ; mkdir -p deps\source\ffmpeg ; mkdir -p deps\build\x264 ; mkdir -p deps\build\ffmpeg ; mkdir -p deps\installed
 
 $VCINSTALLDIR = $(& "${env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vswhere.exe" -latest -property installationPath)
 Add-Content $env:GITHUB_ENV "LIBCLANG_PATH=${VCINSTALLDIR}\VC\Tools\LLVM\x64\bin`n"
