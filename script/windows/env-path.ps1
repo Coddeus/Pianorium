2f89127d2a20dbb253cc0f795c3b6982570c20e8
Coddeus <codeusbonus@gmail.com>
2023-08-27 01:12:20 +0200

    🔧 Relative paths correction

--- script/windows/env-path.ps1
+++ script/windows/env-path.ps1
@@ -1,10 +1,10 @@
 cd D:\a\Pianorium\Pianorium
 
-mkdir -p deps\source\x264 ; mkdir -p deps\source\ffmpeg ; mkdir -p deps\build\x264 ; mkdir -p deps\build\ffmpeg ; mkdir -p deps\installed
+mkdir -p deps\source\ffmpeg ; mkdir -p deps\build\x264 ; mkdir -p deps\build\ffmpeg ; mkdir -p deps\installed
 
 $VCINSTALLDIR = $(& "${env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vswhere.exe" -latest -property installationPath)
 Add-Content $env:GITHUB_ENV "LIBCLANG_PATH=${VCINSTALLDIR}\VC\Tools\LLVM\x64\bin`n"
-# Add-Content $env:GITHUB_ENV "FFMPEG_DIR=${pwd}\deps\installed`n"
+Add-Content $env:GITHUB_ENV "FFMPEG_DIR=${pwd}\deps\installed`n"
 Add-Content $env:GITHUB_PATH "D:\a\_temp\msys64`n"
 Add-Content $env:GITHUB_PATH "D:\a\_temp\msys64\usr\bin`n"
 Add-Content $env:GITHUB_PATH "D:\a\_temp\msys64\mingw64\bin`n"
--- script/windows/x264-ffmpeg.sh
+++ script/windows/x264-ffmpeg.sh
@@ -1,23 +1,25 @@
 cd D:/a/Pianorium/Pianorium
 
-cd deps/source/x264
+cd deps/source
 echo Downloading x264... `date +"%Hh %Mm %Ss"`
 git clone https://code.videolan.org/videolan/x264.git
+cd x264
 curl "http://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;f=config.guess;hb=HEAD" > config.guess
 echo Configuring x264... `date +"%Hh %Mm %Ss"`
 sed -i 's/host_os = mingw/host_os = msys/' configure
-CC=cl configure --prefix=../../installed --enable-static
+cd ../../build/x264
+CC=cl ../../source/x264/configure --prefix=../../installed --enable-static
 echo Making x264... `date +"%Hh %Mm %Ss"`
 make -j 2
 echo Installing x264... `date +"%Hh %Mm %Ss"`
 make install
 
-cd ..
+cd ../../source
 echo Downloading FFmpeg... `date +"%Hh %Mm %Ss"`
 git clone https://git.ffmpeg.org/ffmpeg.git ffmpeg
-cd ffmpeg
+cd ../build/ffmpeg
 echo Configuring FFmpeg... `date +"%Hh %Mm %Ss"`
-CC=cl PKG_CONFIG_PATH=../../installed/lib/pkgconfig configure --prefix=../../installed --toolchain=msvc --target-os=win64 --arch=x86_64 --enable-yasm --enable-asm --disable-shared --enable-static --enable-libx264 --enable-gpl --enable-version3 --disable-debug --enable-optimizations --extra-ldflags="-LIBPATH:../../installed/lib" --extra-cxxflags="-I../../installed/include/ -MTd -Od -Zi" --extra-cflags="-I../../installed/include/ -MTd -Od -Zi"
+CC=cl PKG_CONFIG_PATH=../../installed/lib/pkgconfig ../../source/ffmpeg/configure --prefix=../../installed --toolchain=msvc --target-os=win64 --arch=x86_64 --enable-yasm --enable-asm --disable-shared --enable-static --enable-libx264 --enable-gpl --enable-version3 --disable-debug --enable-optimizations --extra-ldflags="-LIBPATH:../../installed/lib" --extra-cxxflags="-I../../installed/include/ -MTd -Od -Zi" --extra-cflags="-I../../installed/include/ -MTd -Od -Zi"
 echo Making FFmpeg... `date +"%Hh %Mm %Ss"`
 make -j 2
 echo Installing FFmpeg... `date +"%Hh %Mm %Ss"`
