ac6adc478d93fa493dcc5e13ac93eee8d0620fdb
Coddeus <codeusbonus@gmail.com>
2023-08-28 18:00:55 +0200

    🔧 Another try

--- script/windows/env-path.ps1
+++ script/windows/env-path.ps1
@@ -4,7 +4,7 @@ mkdir -p deps\source\ffmpeg ; mkdir -p deps\build\x264 ; mkdir -p deps\build\ffm
 
 $VCINSTALLDIR = $(& "${env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vswhere.exe" -latest -property installationPath)
 Add-Content $env:GITHUB_ENV "LIBCLANG_PATH=${VCINSTALLDIR}\VC\Tools\LLVM\x64\bin`n"
-Add-Content $env:GITHUB_ENV "FFMPEG_DIR=${pwd}\deps\installed`n"
+# Add-Content $env:GITHUB_ENV "FFMPEG_DIR=${pwd}\deps\installed`n"
 Add-Content $env:GITHUB_PATH "D:\a\_temp\msys64`n"
 Add-Content $env:GITHUB_PATH "D:\a\_temp\msys64\usr\bin`n"
 Add-Content $env:GITHUB_PATH "D:\a\_temp\msys64\mingw64\bin`n"
